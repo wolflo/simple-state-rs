db470e757dbae6fe46d46affc2af3fe257101d03
wolflo <33909953+wolflo@users.noreply.github.com>
2021-12-07 12:28:15 -0700

    Break out state, block, runner

--- tests/fns_only.rs
+++ tests/fns_only.rs
@@ -8,14 +8,16 @@ use async_trait::async_trait;
 use linkme::{distributed_slice, DistributedSlice};
 
 pub fn gmain() {
-    run_state(NullState, &STATES_FROM_NULL_STATE, &FNS_ON_NULL_STATE);
+    // run_state(NullState, &STATES_FROM_NULL_STATE, &FNS_ON_NULL_STATE);
+    // run_state(NullState, &STATES_FROM_NULL_STATE, &FNS_ON_NULL_STATE);
+    // dispatch::<S1>
 }
 
 fn run_state<B, S>(base: B, sub_states: &[fn(S)], tests: &[fn(S)])
 where
     S: StateFrom<B> + DevClient + Clone
 {
-    let state: S = S::state_from(base);
+    let state: S = S::new(base);
     let mut snap_id = state.snap();
     for runner in sub_states {
         runner(state.clone());
@@ -29,8 +31,23 @@ where
     }
 }
 
-pub trait StateFrom<T> { fn state_from(src: T) -> Self; }
-impl<T> StateFrom<T> for T { fn state_from(src: T) -> Self { src }}
+pub struct Test<S> {
+    pub name: &'static str,
+    pub run: fn(S),
+}
+
+// fn run_s1(b: NullState) { run_state(b, &STATES_FROM_S1, &FNS_ON_S1) }
+// static _S01: fn(NullState) = run::<S1>;
+
+
+fn dispatch<S, R, P>(prev_state: P) where S: State<Prev=P>, R: Runner<State=S> {
+    let state: S = S::new(prev_state);
+    let runner: R = R::new(state.clone());
+    runner.run();
+}
+
+pub trait StateFrom<T> { fn new(src: T) -> Self; }
+impl<T> StateFrom<T> for T { fn new(src: T) -> Self { src }}
 
 
 #[derive(Debug, Clone)]
@@ -43,15 +60,52 @@ pub static STATES_FROM_NULL_STATE: [fn(NullState)] = [..];
 
 pub trait DevClient {
     // client() needs to be impl by users so we can provide default
-    // impls for snap and reset
+    // impls for snap and reset?
     // fn client(&self) -> DevRpcMiddleware;
     fn snap(&self) -> u64 { 17 }
     fn reset(&self, id: u64) {}
 }
 
+pub trait State: Clone {
+    type Prev: State;
+    fn new(base: Self::Prev) -> Self;
+}
+pub trait Block: Clone {
+    type State: State;
+    fn new(state: Self::State) -> Self;
+    fn state(&self) -> Self::State;
+    fn tests(&self) -> &[Test<Self::State>] { &[] }
+    fn children(&self) -> &[fn(Self::State)] { &[] }
+    fn before_each(&self) { () }
+    fn after_each(&self) { () }
+    fn after(&self) { () }
+}
+pub trait Runner: Block {
+    fn run_tests(&self) {
+        for t in self.tests() {
+            println!("{}", t.name);
+            self.before_each();
+            (t.run)(self.state().clone());
+            self.after_each();
+        }
+        self.after();
+    }
+    fn run_children(&self) {
+        for runner in self.children() {
+            self.before_each();
+            runner(self.state().clone());
+            self.after_each();
+        }
+    }
+    fn run(&self) {
+        self.run_children();
+        self.run_tests();
+    }
+}
+
 #[derive(Debug, Clone)]
 pub struct S1;
-impl StateFrom<NullState> for S1 { fn state_from(s: NullState) -> Self { S1 } }
+impl StateFrom<NullState> for S1 { fn new(s: NullState) -> Self { S1 } }
 
 // macro generated:
 impl DevClient for S1 {}
@@ -63,10 +117,9 @@ pub static STATES_FROM_S1: [fn(S1)] = [..];
 static _S01: fn(NullState) = run_s1;
 fn run_s1(b: NullState) { run_state(b, &STATES_FROM_S1, &FNS_ON_S1) }
 
-
 #[derive(Debug, Clone)]
 pub struct S2;
-impl StateFrom<S1> for S2 { fn state_from(s: S1) -> Self { S2 } }
+impl StateFrom<S1> for S2 { fn new(s: S1) -> Self { S2 } }
 
 impl DevClient for S2 {}
 #[distributed_slice]
